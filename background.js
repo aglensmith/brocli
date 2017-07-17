cbe49f0b978096dc44d4e917a3f124520b0ddef2
A G Smith <aglensmith@gmail.com>
2017-07-17 06:43:08 -0500

    fixed listeners so now onInputEntered opens new tab

--- background.js
+++ background.js
@@ -1,22 +1,17 @@
-<script>
-
-//Sets the description and styling for the default suggestion. The default suggestion is the text that is displayed in the first suggestion row underneath the URL bar.
-
 chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
-    console.log(text);
-    if (text.search('coffee') > -1) {
+    
+    if (text.search('o') > -1) {
         var suggestions = [];
 
-        suggestions.push({ content: 'Coffee - Wikipedia', description: 'Coffee - Wikipedia' });
+        suggestions.push({ content: 'order', description: 'Coffee - Wikipedia' });
         suggestions.push({ content: 'Starbucks Coffee', description: 'Starbucks Coffee' });
 
         suggest(suggestions);
-        console.log(suggestions);
+
     }
 });
 
-chrome.experimental.omnibox.onInputEntered(function(text, disposition) {
-    console.log(text);
-    window.open('http://google.com');
+chrome.omnibox.onInputEntered.addListener(function(text, disposition) {
+
+    chrome.tabs.create({"url": text});
 });
-</script>
