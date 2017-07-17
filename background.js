0844d8d108ec0d8ef1d09e99ad9367285ca45afb
A G Smith <aglensmith@gmail.com>
2017-07-17 17:08:36 -0500

    split logic in onInputEntered into multiple functions: urlToHref, creatTab, etc

--- background.js
+++ background.js
@@ -1,3 +1,34 @@
+function creatTab(url){
+    chrome.tabs.create({
+        'url': url
+    })
+}
+
+function urlToHref (url) {
+    var u = new URL(url);
+    return u.origin;
+}
+
+function getActiveTabUrl (){
+
+    var activeTabUrl;
+    var query = {currentWindow: true, active: true};
+    chrome.tabs.query(query, function (foundTabs) {
+            if (foundTabs.length > 0) {
+                console.log('found tab: ' + foundTabs[0].url);
+                activeTabUrl = foundTabs[0].url; // <--- this is what you are looking for
+                console.log('active tab: ' + activeTabUrl);
+            } 
+            else {
+                // there's no window or no selected tab
+            }
+        }
+    ); 
+    console.log('outer active tab: ' + activeTabUrl);
+    return activeTabUrl;
+}
+
+
 chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
     
     if (text.search('o') > -1) {
@@ -18,28 +49,16 @@ chrome.omnibox.onInputEntered.addListener(function(text, disposition) {
     var orderNum = splitText[1];
 
     //build urls
-    var orderUrl = 'store/admin/orders/orderlist.aspx?ovu=/store/admin/accounting/OrderEdit.aspx%3FOrderID%3D'+orderNum+'&ovw=1&ovn=0';
+    var orderUrl = '/store/admin/orders/orderlist.aspx?ovu=/store/admin/accounting/OrderEdit.aspx%3FOrderID%3D'+orderNum+'&ovw=1&ovn=0';
 
     //execute commands
     if (splitText[0].toLowerCase() == 'order') {
-         
-        chrome.tabs.query(
-            {
-                currentWindow: true,    // currently focused window
-                active: true            // selected tab
-            },
-            function (foundTabs) {
-                if (foundTabs.length > 0) {
-                    var url = foundTabs[0].url; // <--- this is what you are looking for
-                    var loc = new URL(url);
-                    var href = loc.href;
-                    var navto = href + orderUrl;
-                    chrome.tabs.create({"url": navto}); 
-                } 
-                else {
-                    // there's no window or no selected tab
-                }
-            }
-        );
+
+        var activeUrl = getActiveTabUrl();
+        console.log('listener active tab: ' + activeUrl)
+        var href = urlToHref(activeUrl);
+        var navto = href + orderUrl;
+        creatTab(href);
+
     }
 });
