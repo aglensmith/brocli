85d178a3f9145845315e623d23f48ccb3cc0fa02
A G Smith <aglensmith@gmail.com>
2017-07-17 09:56:21 -0500

    completed minimum working order edit command

--- background.js
+++ background.js
@@ -12,6 +12,34 @@ chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
 });
 
 chrome.omnibox.onInputEntered.addListener(function(text, disposition) {
+        
+    //parse input
+    var splitText = text.split(" ");
+    var orderNum = splitText[1];
 
-    chrome.tabs.create({"url": text});
+    //build urls
+    var orderUrl = 'store/admin/orders/orderlist.aspx?ovu=/store/admin/accounting/OrderEdit.aspx%3FOrderID%3D'+orderNum+'&ovw=1&ovn=0';
+
+    //execute commands
+    if (splitText[0].toLowerCase() == 'order') {
+         
+        chrome.tabs.query(
+            {
+                currentWindow: true,    // currently focused window
+                active: true            // selected tab
+            },
+            function (foundTabs) {
+                if (foundTabs.length > 0) {
+                    var url = foundTabs[0].url; // <--- this is what you are looking for
+                    var loc = new URL(url);
+                    var href = loc.href;
+                    var navto = href + orderUrl;
+                    chrome.tabs.create({"url": navto}); 
+                } 
+                else {
+                    // there's no window or no selected tab
+                }
+            }
+        );
+    }
 });
