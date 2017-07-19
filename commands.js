604b345519e4d226a2bbd756444c3153f1ad5033
A G Smith <aglensmith@gmail.com>
2017-07-19 17:21:09 -0500

    added parser for product commands and added switch statement to runCommands()

--- commands.js
+++ commands.js
@@ -1,7 +1,9 @@
-//comamands.js
-//['order', '-e', '123456']
+/**
+ * @fileOverview Uses optparse.js to parse command switches
+ * 
+ */
 
-//create multiple parser for each command
+
 
 //Might use for initial parse
 var initialSwitches = [
@@ -9,11 +11,24 @@ var initialSwitches = [
 ];
 var initialParser = new optparse.OptionParser();
 
+var entities = [
+    "category",
+    "customer",
+    "order",
+    "product"
+];
+
+var options = {
+    entity: undefined,
+    action: undefined,
+    id: undefined,
+};
+
+
 /** 
 * Order Commands
 *
 */
-
 var orderSwitches = [
     ['-v', '--view [ID]', 'view orders'],
     ['-e','--edit [ID]', 'edit orders'],
@@ -22,24 +37,62 @@ var orderSwitches = [
 var orderParser = new optparse.OptionParser(orderSwitches);
 
 orderParser.on('view', function (name, value) {
-    var orderUrl = '/store/admin/orders/orderlist.aspx?ovu=/store/admin/orders/viewOrder.aspx%3ForderID%3D'+value+'&ovw=1&ovn=0';
+    var orderUrl = '/store/admin/orders/orderlist.aspx?ovu=/store/admin/orders/viewOrder.aspx%3ForderID%3D'+value;
     navCurrentDomain(orderUrl);
 });
 
 orderParser.on('edit', function (name, value) {
-    var orderUrl = '/store/admin/orders/orderlist.aspx?ovu=/store/admin/accounting/OrderEdit.aspx%3FOrderID%3D'+value+'&ovw=1&ovn=0';
+    var orderUrl = '/store/admin/orders/orderlist.aspx?ovu=/store/admin/accounting/OrderEdit.aspx%3FOrderID%3D'+value;
     navCurrentDomain(orderUrl);
 });
 
 orderParser.on('audit', function (name, value) {
-    var orderUrl = '/store/admin/orders/orderlist.aspx?ovu=/store/admin/orders/ViewOrder.aspx%3ForderID%3D'+value+'&ovw=1&ovn=0';
+    var orderUrl = '/store/admin/orders/orderlist.aspx?ovu=/store/admin/orders/ViewOrder.aspx%3ForderID%3D'+value;
     navCurrentDomain(orderUrl);
 });
 
-function runCommands (commands) {
-    console.log(commands)
-    orderParser.parse(commands)
-}
+orderParser.on(function(opt) {
+	console.log('No handler was defined for option:' +  opt);
+});
 
-//at end executeCommands function with all arg parser.parse(ARGS);
-// first argument determines which parser is used?
+orderParser.on('*', function(opt, value) {
+    console.log('wild handler for ' + opt + ', value=' + value);
+});
+
+
+/** 
+* Product Commands
+*
+*/
+var productSwitches = [
+    ['-e','--edit [ID]', 'edit products'],
+    ['-a', '--audit [ID]', 'audit products'],
+]
+
+var productParser = new optparse.OptionParser(productSwitches);
+
+productParser.on('edit', function (name, value) {
+    var productUrl = '/store/admin/products/listproducts.aspx?ovu=/store/admin/products/productedit/general.aspx%3FcatID%3D21%26ID%3D'+value+'&ovw=0&ovn=1';
+    navCurrentDomain(productUrl);
+});
+
+productParser.on('audit', function (name, value) {
+    var orderUrl = '/store/admin/products/listproducts.aspx?ovu=/store/admin/site/entityaudithistory.aspx%3FEntityID%3D36%26EntityTypeID%3D'+value;
+});
+
+
+
+function runCommands (commands) {
+
+    switch (commands[0]) {
+        case 'order':
+            //note to self: break is optional
+            orderParser.parse(commands);
+            break;
+        case 'product':
+            productParser.parse(commands);
+            break;
+
+    }
+    
+}
