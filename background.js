e7617a8ab6f23a1d69edb90a4c5a416d16c7fce0
A G Smith <aglensmith@gmail.com>
2017-07-19 19:43:53 -0500

    moved creatTab(), urlOrigin(), and navCurrentDomain() to helpers.js

--- background.js
+++ background.js
@@ -1,25 +1,5 @@
 
-//Move these functions to a helpers.js file
-function creatTab (url) {
-    chrome.tabs.create({
-        'url': url
-    })
-}
 
-function urlOrigin (url) {
-    var u = new URL(url);
-    return u.origin;
-}
-
-function navCurrentDomain (relativePath) {
-    var query = {currentWindow: true, active: true};
-    chrome.tabs.query(query, function (results) {
-        if (results.length > 0) {
-            var activeTabOrigin = urlOrigin(results[0].url);
-            creatTab(activeTabOrigin + relativePath)
-        } 
-    }); 
-}
 
 chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
     
