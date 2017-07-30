3a311eb9adbabf9d033ae095aa832f4cebd923eb
A G Smith <aglensmith@gmail.com>
2017-07-30 10:02:37 -0500

    added productAudit URL and changed pageType switch to use concat() instead of +

--- ac-urls.js
+++ ac-urls.js
@@ -2,6 +2,7 @@
  * Stuff for building AC urls for navigation
  */
 
+
 var listOf = {
     'blogs':'/store/admin/content/blogs/bloglist.aspx',
     'blogPosts':'/store/admin/content/blogs/blogpostlist.aspx',
@@ -20,73 +21,73 @@ var listOf = {
     'urlRedirects':'/Store/Admin/Settings/Global/RedirectURLList.aspx?'
 };
 
-//settings
 settingsPage = {
     'basicInformation': '/Store/Admin/Settings/Store/StoreInformation.aspx'
 };
 
-
-//customers
-var customerEdit = [
-    '/store/admin/customers/customerlist.aspx?ovu=/store/admin/customers/CustomerEdit.aspx%3FID%3D',
-    '&ovw=0&ovn=1'
-];
-
-//discounts
-var discountEdit = [
-    '/store/admin/marketing/discounts/discountlist.aspx?ovu=/store/admin/marketing/discounts/DiscountEdit.aspx%3FDiscountMethodID%3D',
-    '&ovw=0&ovn=0'
-];
-
-//orders
-var orderEdit = [
-    '/store/admin/orders/orderlist.aspx?ovu=/store/admin/accounting/OrderEdit.aspx%3FOrderID%3D',
-    '&ovw=1&ovn=0'
-];
-
+//Audit Pages
 var orderAudit = [
     '/store/admin/orders/orderlist.aspx?ovu=/store/admin/accounting/OrderEdit.aspx%3FOrderID%3D',
     '+/store/admin/site/EntityAuditHistory.aspx%3FEntityID%3D',
     '%26EntityTypeID%3D300&ovw=1+1&ovn=0+0'
 ];
 
-//products
+// Edit Pages
+var customerEdit = [
+    '/store/admin/customers/customerlist.aspx?ovu=/store/admin/customers/CustomerEdit.aspx%3FID%3D',
+    '&ovw=0&ovn=1'
+];
+
+var discountEdit = [
+    '/store/admin/marketing/discounts/discountlist.aspx?ovu=/store/admin/marketing/discounts/DiscountEdit.aspx%3FDiscountMethodID%3D',
+    '&ovw=0&ovn=0'
+];
+var orderEdit = [
+    '/store/admin/orders/orderlist.aspx?ovu=/store/admin/accounting/OrderEdit.aspx%3FOrderID%3D',
+    '&ovw=1&ovn=0'
+];
 var productEdit = [
     '/store/admin/products/listproducts.aspx?ovu=/store/admin/products/productedit/general.aspx%3FID%3D',
     '&ovw=0&ovn=1'
 ];
 
-var edit = {
-    'product': productEdit
-};
 
-var orderView;
+'https://austintest.americommerce.com/store/admin/products/listproducts.aspx?EntityTypeID=30&ovu=/store/admin/products/productedit/general.aspx%3FID%3D42+/store/admin/site/entityaudithistory.aspx%3FEntityID%3D42%26EntityTypeID%3D30'
 
-function editEntity (str1, str2, str3) {
-    return str1.concat(str2, str3);
+
+// Audit Pages
+var productAudit = [
+    'https://austintest.americommerce.com/store/admin/products/listproducts.aspx?EntityTypeID=30&ovu=/store/admin/products/productedit/general.aspx%3FID%3D',
+    '+/store/admin/site/entityaudithistory.aspx%3FEntityID%3D',
+    '%26EntityTypeID%3D30'
+]
+
+var audit = {
+    'order': orderAudit,
+    'product': productAudit
 }
 
-function buildAcPath(name, value) {
-    
-    //split to get page type. Ex product.edit
+var edit = {
+    'customer': customerEdit,
+    'discount': discountEdit,
+    'product': productEdit,
+    'order': orderEdit
+};
 
-    var s = name.split('.');
+function buildAcPath(name, value) {
+    //split switch name to get page type. Ex product-edit
+    var s = name.split('-');
     var pageType = s[s.length - 1];
     var entity = s[0];
     var path;
-    console.log('builpaths:');
-    console.log(name);
-    console.log(s);
-    console.log(pageType);
-    console.log(entity);
-    console.log(path);
+
     switch (pageType) {
         case 'list':
             path = listOf[value];
             break;
         case 'edit':
             parts = edit[entity];
-            path = parts[0] + value + parts[1];
+            path = parts[0].concat(value, parts[1]);
             break;
     } 
     return path;
