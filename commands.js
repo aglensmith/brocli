539c3d8dc22db518d98b28f668f699beeb36d116
A G Smith <aglensmith@gmail.com>
2017-07-29 21:47:45 -0500

    added logging and working logic for runAcCommands()

--- commands.js
+++ commands.js
@@ -12,21 +12,19 @@
  * AC Commands
  * 
  */
-
 var acSwitches = [
-    ['-c', '--customeredit [ID]', 'customer edit'],
+    ['-c', '--customer.edit [ID]', 'customer edit'],
     ['-cd', '--changedir [LOC]', 'change directories'],
-    ['-ca', '--customeraudit [ID]', 'customer audit'],
-    ['-cat', '--categoryedit [ID]', 'category edit'],
-    ['-catv', '--categoryview [ID]', 'category view'],
-    ['-cata', '--categoryaudit [ID]', 'category audit'],
-    ['-d', '--categoryaudit [ID]', 'category audit'],
-    ['-p','--productedit [ID]', 'product edit'],
-    ['-o','--orderedit [ID]', 'order edit'],
-    ['-oa','--orderaudit [ID]', 'order audit'],
-    ['-ov','--orderview [ID]', 'order view'],
-    ['-ov','--orderview [ID]', 'order view'],
-    ['-ls','--list [ENTITY]', 'list entity']
+    ['-ca', '--customer.audit [ID]', 'customer audit'],
+    ['-cat', '--category.edit [ID]', 'category edit'],
+    ['-catv', '--category.view [ID]', 'category view'],
+    ['-cata', '--category.audit [ID]', 'category audit'],
+    ['-d', '--category.audit [ID]', 'category audit'],
+    ['-p','--product.edit [ID]', 'product edit'],
+    ['-o','--order.edit [ID]', 'order edit'],
+    ['-oa','--order.audit [ID]', 'order audit'],
+    ['-ov','--order.view [ID]', 'order view'],
+    ['-l','--list [ENTITY]', 'list entity']
 ];
 
 var acParser = new optparse.OptionParser(acSwitches);
@@ -39,29 +37,51 @@ var options = {
 
 //domain -- set to domain, if domain, otherwise set to current tab location
 acParser.on(0, function (name, value) {
-    options[domain] = value;
+    options.domain = value;
 });
 
-acParser.on('customeredit', function (name, value) {
-    options[action] = name;
-    options[entityId] = value;
-    option[path] = buildAcPath(name);
+acParser.on('customer.edit', function (name, value) {
+    options.action = name;
+    options.path = buildAcPath(name, value);
 });
 
-function runCommands (commands) {
-    switch (commands[0]) {
-        case 'order':
-            //note to self: break is optional
-            orderParser.parse(commands);
-            break;
-        case 'product':
-            productParser.parse(commands);
-            break;
-    }   
-}
+acParser.on('order.edit', function (name, value) {
+    options.action = name;
+    options.path = buildAcPath(name, value);
+});
+
+acParser.on('product.edit', function (name, value) {
+    options.action = name;
+    options.path = buildAcPath(name, value);
+});
+
+acParser.on('list', function (name, value) {
+    options.action = name;
+    options.path = buildAcPath(name, value);
+    console.log("hello from list");
+    console.log(name);
+    console.log(value);
+});
 
 function runAcCommands (commands) {
-    acParser.parse(commands)
-    var domain = options[domain] || "";
-    var relUrl = buildUrl ()
+
+    acParser.parse(commands);
+    console.log(commands);
+    var domainPresent = options.domain || "";
+    var path = options.path;
+    var url = domainPresent + path;
+    console.log('test1');
+    console.log(options.domain);
+    console.log(options.path);
+    console.log(options.action)
+    console.log(url);
+    if (domainPresent) {
+        goTo(url);
+    } else {
+        goTo(url);
+    }
+    console.log('test2');
+    console.log(options.domain);
+    console.log(options.path);
+    console.log(url);
 }
