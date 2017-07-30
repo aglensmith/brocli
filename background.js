12012ea2e6e1b7851fc2416f7b1559d573741f83
A G Smith <aglensmith@gmail.com>
2017-07-30 10:00:20 -0500

    added logic for pushing option suggestions in onInputChanged

--- background.js
+++ background.js
@@ -1,36 +1,22 @@
 /**
  * @fileOverview Event listeners that listen and respond to Chrome events
  */
+
+
 var activeTabOrigin = "";
 chrome.omnibox.onInputChanged.addListener(function (text, suggest) { 
     var suggestions = [];
     var query = {currentWindow: true, active: true};
-    
-    chrome.tabs.query(query, function (results) {
-        if (results.length > 0) {
-            activeTabOrigin = urlOrigin(results[0].url);
-            suggestions.push({ content: activeTabOrigin + " " + text, description: "Run Command: " + activeTabOrigin + " " + text});
-            
-            chrome.omnibox.setDefaultSuggestion({description:suggestions[0].description});
-            // Remove the first suggestion from the array since we just suggested it
-            
-            suggest(suggestions);
-        }
+    var splitText = text.split(" ");
+    acParser.parse(splitText);
+    chrome.omnibox.setDefaultSuggestion({description:'[site] (-pagetype) [id|entity]'});
+    options.suggestions.forEach(function(sug) {
+        suggestions.push({content: sug, description: sug})
     });
-
-    
-    //if (text.search('o') > -1) {
-        //var suggestions = [];
-        //suggestions.push({ content: 'order', description: 'Coffee - Wikipedia' });
-        //suggestions.push({ content: 'Starbucks Coffee', description: 'Starbucks Coffee' });
-        //suggest(suggestions);
-    //}
+    suggest(suggestions);
 });
 
 chrome.omnibox.onInputEntered.addListener(function(text, disposition) {
     var splitText = text.split(" ");
     runAcCommands(splitText);
-    console.log('test3')
-    console.log(options.domain);
-    console.log(options.path);
 });
