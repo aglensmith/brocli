5de348320a6c9afe2be2ce62ad34ab7abac42414
A G Smith <aglensmith@gmail.com>
2017-07-29 21:48:18 -0500

    added logging

--- background.js
+++ background.js
@@ -1,20 +1,36 @@
 /**
  * @fileOverview Event listeners that listen and respond to Chrome events
  */
-
-
+var activeTabOrigin = "";
 chrome.omnibox.onInputChanged.addListener(function (text, suggest) { 
-    if (text.search('o') > -1) {
-        var suggestions = [];
+    var suggestions = [];
+    var query = {currentWindow: true, active: true};
+    
+    chrome.tabs.query(query, function (results) {
+        if (results.length > 0) {
+            activeTabOrigin = urlOrigin(results[0].url);
+            suggestions.push({ content: activeTabOrigin + " " + text, description: "Run Command: " + activeTabOrigin + " " + text});
+            
+            chrome.omnibox.setDefaultSuggestion({description:suggestions[0].description});
+            // Remove the first suggestion from the array since we just suggested it
+            
+            suggest(suggestions);
+        }
+    });
 
-        suggestions.push({ content: 'order', description: 'Coffee - Wikipedia' });
-        suggestions.push({ content: 'Starbucks Coffee', description: 'Starbucks Coffee' });
-
-        suggest(suggestions);
-    }
+    
+    //if (text.search('o') > -1) {
+        //var suggestions = [];
+        //suggestions.push({ content: 'order', description: 'Coffee - Wikipedia' });
+        //suggestions.push({ content: 'Starbucks Coffee', description: 'Starbucks Coffee' });
+        //suggest(suggestions);
+    //}
 });
 
 chrome.omnibox.onInputEntered.addListener(function(text, disposition) {
     var splitText = text.split(" ");
-    runCommands(splitText);
+    runAcCommands(splitText);
+    console.log('test3')
+    console.log(options.domain);
+    console.log(options.path);
 });
