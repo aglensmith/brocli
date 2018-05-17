5b40eefca2275a17b8d5b298fb99ac1a78adff8f
A G Smith <aglensmith@gmail.com>
2018-05-16 19:45:29 -0500

    add formatxml to helpers.js

--- brocli/helpers.js
+++ brocli/helpers.js
@@ -87,3 +87,35 @@ function navCurrentDomain (relativePath) {
     }); 
 }
 
+/*
+* Pretty Print XML
+* Credit: https://gist.github.com/sente/1083506/d2834134cd070dbcc08bf42ee27dabb746a1c54d
+*/
+function formatXml(xml) {
+    var formatted = '';
+    var reg = /(>)(<)(\/*)/g;
+    xml = xml.replace(reg, '$1\r\n$2$3');
+    var pad = 0;
+    jQuery.each(xml.split('\r\n'), function(index, node) {
+        var indent = 0;
+        if (node.match( /.+<\/\w[^>]*>$/ )) {
+            indent = 0;
+        } else if (node.match( /^<\/\w/ )) {
+            if (pad != 0) {
+                pad -= 1;
+            }
+        } else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
+            indent = 1;
+        } else {
+            indent = 0;
+        }
+        var padding = '';
+        for (var i = 0; i < pad; i++) {
+            padding += '  ';
+        }
+        formatted += padding + node + '\r\n';
+        pad += indent;
+    });
+    return formatted;
+}
+
