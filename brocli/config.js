f4b97b75dbb02f8ca7cec57f7e8ae6f5b53e5f76
Austin Smith <asmith@americommerce.com>
2018-05-27 14:32:10 -0500

    wip commit for command folders

--- brocli/config.js
+++ brocli/config.js
@@ -1,6 +1,34 @@
 // This will become options page at some point
 // Or, set values from storage/sync. 
 
+var defaultCommandFolder = "BrocliCommands";
+
+function findCommandFolderId (folder) {
+    chrome.bookmarks.search(folder, function(res){
+        chrome.storage.local.set({'brocliCommandFolderId': res[0].id}, function() {
+            console.log("brocli: command folder found. Set to " + res[0].title + " ID " + res[0].id);
+        });
+    });
+}
+
+
+// if there's one set in stoage, get it; if not, search and set.
+function setCommandFolder () {
+    chrome.storage.local.get('brocliCommandFolderId', function (items) {
+        if (items.brocliCommandFolderId < 1) {
+            findCommandFolderId(defaultCommandFolder);
+        }
+    });
+}
+
+// chrome.bookmarks.getSubTree("734", function(res){console.log(res)});
+// returns array of objects. Each object has children property. If child is a link
+// it will have a url property. Recursively build a list of commands mapped to the url.
+// and store in storage as a cache. 
+function getBookmarkCommands () {
+
+}
+
 function getParams (urlString) {
     var url = new URL(urlString);
     var params = {};
@@ -20,3 +48,5 @@ var defaultSearch = new URL("https://www.google.com/search?q=%s&{google:RLZ}{goo
 var defaultSearchPath = defaultSearch.origin + defaultSearch.pathname;
 var defaultSearchUrl = defaultSearch.href;
 var searchParam = getParams(defaultSearch.href)['%s'];
+
+setCommandFolder();
